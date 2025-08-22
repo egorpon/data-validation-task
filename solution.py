9e095e4ef128640dc94b2d175440ba1fe9b3a6a5
egorpon <ponomarenkoegor36@gmail.com>
2025-08-22 15:37:20 +0300

    add handling invalid date_of_birth in User

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import json
 from abc import ABC, abstractmethod
 from datetime import date
 import re
-from  datetime import datetime
+from datetime import datetime
 
 
 class Validationable(ABC):
@@ -25,36 +25,36 @@ class Address(Validationable):
     city: str
     postal_code: str
 
-    def __init__(self, data: dict) -> None: 
-        self.country = data.get('country','')
-        self.city = data.get('city','')
-        self.postal_code = data.get('postal_code','')
+    def __init__(self, data: dict) -> None:
+        self.country = data.get("country", "")
+        self.city = data.get("city", "")
+        self.postal_code = data.get("postal_code", "")
 
-    def __repr__(self) -> str: 
+    def __repr__(self) -> str:
         return f"{self.country}, {self.city}, {self.postal_code}"
 
-    def is_valid(self) -> bool: 
-        return all([self.is_city_valid(), self.is_country_valid(), self.is_postal_code_valid()])
+    def is_valid(self) -> bool:
+        return all(
+            [self.is_city_valid(), self.is_country_valid(), self.is_postal_code_valid()]
+        )
 
-    def get_invalid_fields(self) -> list[str]: 
+    def get_invalid_fields(self) -> list[str]:
         invalid_fields = {
-            "city":self.is_city_valid(),
-            "country":self.is_country_valid(),
-            "postal_code":self.is_postal_code_valid()}
-        
+            "city": self.is_city_valid(),
+            "country": self.is_country_valid(),
+            "postal_code": self.is_postal_code_valid(),
+        }
+
         return [field for field, value in invalid_fields.items() if not value]
 
-    def is_country_valid(self) -> bool: 
-        return bool(re.fullmatch(r'^[A-Z][a-z]+$',self.country))
-        
+    def is_country_valid(self) -> bool:
+        return bool(re.fullmatch(r"^[A-Z][a-z]+$", self.country))
 
     def is_city_valid(self) -> bool:
-        return bool(re.fullmatch(r'^[A-Z][a-z]+$',self.city))
-            
+        return bool(re.fullmatch(r"^[A-Z][a-z]+$", self.city))
 
-    def is_postal_code_valid(self) -> bool: 
-        return bool(re.fullmatch(r'^\d{5}$',self.postal_code))
-
+    def is_postal_code_valid(self) -> bool:
+        return bool(re.fullmatch(r"^\d{5}$", self.postal_code))
 
 
 class User(Validationable):
@@ -65,60 +65,63 @@ class User(Validationable):
     date_of_birth: date
     addresses: list[Address]
 
-    def __init__(self, data: dict) -> None: 
-        self.email = data.get('email','')
-        self.full_name = data.get('full_name','')
-        self.gender = data.get('gender','')
-        self.addresses = [Address(addr) for addr in data.get('addresses',[])]
-        dob_str = data.get('date_of_birth','')
+    def __init__(self, data: dict) -> None:
+        self.email = data.get("email", "")
+        self.full_name = data.get("full_name", "")
+        self.gender = data.get("gender", "")
+        self.addresses = [Address(addr) for addr in data.get("addresses", [])]
+        dob_str = data.get("date_of_birth", "")
         if dob_str:
-            self.date_of_birth = datetime.strptime(dob_str, '%Y-%m-%d').date()
+            self.date_of_birth = datetime.strptime(dob_str, "%Y-%m-%d").date()
         else:
-            self.date_of_birth = None
+            raise ValueError("Date of birth not provided")
 
-    def __repr__(self) -> str: 
-        return (f"Email: {self.email}\n"
-                f"Full name: {self.full_name}\n" 
-                f"Gender: {self.gender}\n"
-                f"Date of birth: {self.date_of_birth}\n"
-                f"Addresses:\n     {'\n     '.join(str(addr) for addr in self.addresses)}")
-    
-    def is_valid(self) -> bool: 
-        return all([self.is_email_valid(),self.is_full_name_valid(), self.is_gender_valid(), self.is_date_of_birth_valid(), self.is_addresses_valid()])
+    def __repr__(self) -> str:
+        return (
+            f"Email: {self.email}\n"
+            f"Full name: {self.full_name}\n"
+            f"Gender: {self.gender}\n"
+            f"Date of birth: {self.date_of_birth}\n"
+            f"Addresses:\n     {'\n     '.join(str(addr) for addr in self.addresses)}"
+        )
 
-    def get_invalid_fields(self) -> list[str]: 
+    def is_valid(self) -> bool:
+        return all(
+            [
+                self.is_email_valid(),
+                self.is_full_name_valid(),
+                self.is_gender_valid(),
+                self.is_date_of_birth_valid(),
+                self.is_addresses_valid(),
+            ]
+        )
+
+    def get_invalid_fields(self) -> list[str]:
         invalid_fields = {
-            "email" : self.is_email_valid(),
+            "email": self.is_email_valid(),
             "full_name": self.is_full_name_valid(),
-            "gender" : self.is_gender_valid(),
+            "gender": self.is_gender_valid(),
             "date_of_birth": self.is_date_of_birth_valid(),
-            "addresses": self.is_addresses_valid()
-
+            "addresses": self.is_addresses_valid(),
         }
         return [field for field, value in invalid_fields.items() if not value]
 
+    def is_email_valid(self) -> bool:
+        return bool(re.fullmatch(r"^\w{2,}@[a-zA-z]{2,}\.[a-zA-z]{2,}$", self.email))
 
-    def is_email_valid(self) -> bool: 
-        return bool(re.fullmatch(r'^\w{2,}@[a-zA-z]{2,}\.[a-zA-z]{2,}$',self.email))
-       
+    def is_full_name_valid(self) -> bool:
+        return bool(re.fullmatch(r"^[a-zA-z]+\s[a-zA-z]+$", self.full_name))
 
-
-    def is_full_name_valid(self) -> bool: 
-        return bool(re.fullmatch(r'^[a-zA-z]+\s{1}+[a-zA-z]+$',self.full_name))
-            
-
-
-    def is_gender_valid(self) -> bool: 
-        return self.gender in ['male', 'female']
-
-
+    def is_gender_valid(self) -> bool:
+        return self.gender in ["male", "female"]
 
     def is_date_of_birth_valid(self) -> bool:
-        return self.date_of_birth is not None and self.date_of_birth < datetime.now().date()
-            
+        return (
+            self.date_of_birth is not None
+            and self.date_of_birth < datetime.now().date()
+        )
 
-
-    def is_addresses_valid(self) -> bool: 
+    def is_addresses_valid(self) -> bool:
         return all([address.is_valid() for address in self.addresses])
 
 
@@ -126,4 +129,3 @@ def load_records(filename: str) -> list[User]:
     with open(filename) as f:
         data = json.load(f)
         return [User(user) for user in data]
-    
